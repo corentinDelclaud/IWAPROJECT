6f960a46ef85386521506036b7aa58061a927fba
shane donnelly <shane@donnelly>
2025-11-10 14:45:54 +0100

    employee exemple removed

--- back/microservice-transaction/src/main/java/iwaproject/transaction/repository/EmployeeRepository.java
+++
@@ -1,6 +0,0 @@
-package iwaproject.transaction.repository;
-import iwaproject.transaction.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {}
